c5cc8f47a926f3fac8e2d6b54882a2ad76bd77a1
Karel Frajtak <kfrajtak@gmail.com>
2015-05-18 23:43:52 +0100

    code comments added, some minor changes

--- Dev5.CodeFix.Analyzers/Dev5.CodeFix.Analyzers/DisposeObjectsBeforeLosingScopeRule.cs
+++ Dev5.CodeFix.Analyzers/Dev5.CodeFix.Analyzers/DisposeObjectsBeforeLosingScopeRule.cs
@@ -20,15 +20,29 @@ namespace Dev5.CodeFix.Analyzers
         {
             get { return ImmutableArray.Create(Rule); }
         }
+
         public override void Initialize(AnalysisContext context)
         {
             context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.VariableDeclaration);
             context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.SimpleAssignmentExpression);
         }
+
+        /// <summary>
+        /// Gets type symbol for System.IDisposable interface.
+        /// </summary>
+        /// <param name="compilation"></param>
+        /// <returns></returns>
         public static INamedTypeSymbol IDisposable(Compilation compilation)
         {
             return compilation.GetTypeByMetadataName("System.IDisposable");
         }
+
+        /// <summary>
+        /// Returns boolean value indicating if the <paramref name="typeInfo"/> implements System.IDisposable interface.
+        /// </summary>
+        /// <param name="typeInfo">TypeInfo to check</param>
+        /// <param name="compilation"></param>
+        /// <returns></returns>
         private static bool IsDisposable(TypeInfo typeInfo, Compilation compilation)
         {
             if(typeInfo.Type == null)
@@ -37,28 +51,32 @@ namespace Dev5.CodeFix.Analyzers
             }
             return !typeInfo.Type.IsValueType && typeInfo.Type.AllInterfaces.Any(i => i.Equals(IDisposable(compilation)));
         }
+
         private void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
             var semanticModel = context.SemanticModel;
             var compilation = context.SemanticModel.Compilation;
+            // are we inside using block? i.e. is the Parent of current node UsingStatement
             var insideUsingStatement = context.Node.Parent is UsingStatementSyntax;
 
             var declaration = context.Node as VariableDeclarationSyntax;
+            // variable declaration node
             if (declaration != null)
             {
+                // more than one variable can be declared
                 foreach (var declarator in declaration.Variables)
                 {
                     var variable = declarator.Identifier;
                     var variableSymbol = semanticModel.GetDeclaredSymbol(declarator);
                     var eq = declarator.Initializer as EqualsValueClauseSyntax;
                     var varTypeInfo = semanticModel.GetTypeInfo(eq?.Value);
-                    if (!IsDisposable(varTypeInfo, compilation) 
-                        || (IsDisposable(varTypeInfo, compilation) && insideUsingStatement))
+                    // non-disposable variable is declared or currently inside using block
+                    if (!IsDisposable(varTypeInfo, compilation) || insideUsingStatement)
                     {
                         continue;
                     }
 
-                    var kind = semanticModel.GetDeclaredSymbol(declarator).Kind;
+                    // report this
                     context.ReportDiagnostic(Diagnostic.Create(Rule, declarator.GetLocation()));
                 }
                 return;
@@ -67,20 +85,22 @@ namespace Dev5.CodeFix.Analyzers
             var assignment = context.Node as AssignmentExpressionSyntax;
             if (assignment != null)
             {
-                var rightTypeInfo = semanticModel.GetTypeInfo(assignment.Right);                
-
-                if (!IsDisposable(rightTypeInfo, compilation))
+                // does the type of the RHS node implement IDisposable?
+                var typeInfo = semanticModel.GetTypeInfo(assignment.Right);                
+                if (!IsDisposable(typeInfo, compilation))
                 {
                     return;
                 }
 
                 var identifier = assignment.Left as IdentifierNameSyntax;
                 var kind = semanticModel.GetSymbolInfo(identifier).Symbol;
-                if(kind?.Kind == SymbolKind.Field || insideUsingStatement)
+                // assigning field value or currently inside using block
+                if (kind?.Kind == SymbolKind.Field || insideUsingStatement)
                 {
                     return;
                 }
-                
+
+                // report this
                 context.ReportDiagnostic(Diagnostic.Create(Rule, assignment.GetLocation()));
                 return;
             }
